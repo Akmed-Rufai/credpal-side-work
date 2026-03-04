26067baf79a42dfe3998a51128319963dca82680
Hezekiah Oyemade <oyedey@gmail.com>
2026-03-04 14:47:19 +0100

    Project and Database setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
+import { OrganizationsModule } from './modules/organizations/organizations.module';
+import { ProgramsModule } from './modules/programs/programs.module';
+import { EnrollmentsModule } from './modules/enrollments/enrollments.module';
+import { PaymentsModule } from './modules/payments/payments.module';
+import { WebhooksModule } from './modules/webhooks/webhooks.module';
+import { CertificatesModule } from './modules/certificates/certificates.module';
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    UsersModule,
+    OrganizationsModule,
+    ProgramsModule,
+    EnrollmentsModule,
+    PaymentsModule,
+    WebhooksModule,
+    CertificatesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
